ee9615af743e39c8ec2a48e62c69ca9508f408af
Daiana Ribeiro <daiana.arruda.ribeiro@gmail.com>
2026-03-15 22:30:24 -0300

    Sugestão: o campo email deve possuir a restrição UNIQUE, pois dois clientes não devem possuir o mesmo e-mail no sistema.

--- criacao_clientes.sql
+++ criacao_clientes.sql
@@ -1,7 +1,7 @@
 CREATE TABLE clientes (
     id_cliente INT PRIMARY KEY,
     nome VARCHAR(100),
-    cpf VARCHAR(11),
+    email VARCHAR(100) UNIQUE,
     email VARCHAR(100),
     data_cadastro DATE
 );
